2b87389817f1075c763873d3f46b4536c9f46f01
Senal Bulumulle <senal.bulumulle@gmail.com>
2022-05-24 16:03:08 -0400

    Added temporary file: [index-new.php]

---
+++ index-new.php
@@ -0,0 +1,25 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<?php 
+include 'System_Files/include-ui.php';
+
+include 'System_Files/system.home.screen.banner.php';
+
+include 'src/avakasaya/space21.php';
+    
+
+include 'src/system.home.screen.sub.menu.banner.php';
+
+
+include 'src/avakasaya/space2.php';
+
+        
+
+include 'src/avakasaya/space7.php';
+
+
+include 'System._Files/system.home.screen.sub.menu.popup.buttons.php';
+        
+include 'System_Files/system.home.screen.foil.bar.settings.panel.php';
+
+
+?>
