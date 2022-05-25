e7346ba96fbaabd1d458b6830002925bd2d38c38
Senal Bulumulle <senal.bulumulle@gmail.com>
2022-05-25 15:46:51 -0400

    Organized some stuff...

--- index.php
+++ index.php
@@ -1,92 +1,16 @@
-<!-- CLOCK.HTML-->
+<!-- 
+    index.php
 
-<link rel="stylesheet" href="../src/UI.css" type="text/css">
+    A minimalist digital clock for Smith-Pad Web Interface. 
+-->
 
-<link rel="stylesheet" href="../src/position-text.css" type="text/css">
-<link rel="stylesheet" href="../src/index.css" type="text/css">
-<div class="bar">
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
+<?php include 'System_Files/system.global.app.include.ui.php'; ?>
 
+<?php include 'src/avakasaya/space6.php'; ?>
+<?php include 'System_Files/system.app.clock.back.button.php'; ?>
 
-<a href = "#" class="button-no-animate-mode">BACK</a>
-</div>
-<h1> 
-<style> 
-body{
-  background: rgb(0, 0, 0);
-}
-
-</style>
-<div class="space"></div>
-<div class="space"></div>
-
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-
-
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-
-
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>
-<div class="space"></div>   
-<div class="center">
-<fontsize200> 
-    <fontcolorWHITE>
-
-    <div id="senalClock"></div>
-
-    <script type="text/javascript">
-        function GetClock() {
-            var d = new Date();
-            var senalhour = d.getHours(),
-                senalmin = d.getMinutes();
-            if (senalmin <= 9) senalmin = "0" + senalmin
-
-            var senal_clock_text = "" + senalhour + ":" + senalmin + "";
-            document.getElementById('senalClock').innerHTML = senal_clock_text;
-        }
-
-        GetClock();
-        setInterval(GetClock, 1000);
-    </script>
-
-</fontsize200>
-</fontcolorWHITE>
-
-</div>
+<?php include 'src/avakasaya/space2.php'; ?>
+<?php include 'src/avakasaya/space16.php'; ?>
+<?php include 'src/avakasaya/space10.php'; ?>
+<?php include 'src/avakasaya/space10.php'; ?>
+<?php include 'System_Files/system.app.clock.content.php'; ?>
