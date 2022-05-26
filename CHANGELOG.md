1caea7734b1db70c7e2985b6ecc452c1c0a70318
senalbulumulle <senal.bulumulle@gmail.com>
2022-05-26 13:38:46 -0400

    Re-organized code in PHP in [APPS-Calculator] branch

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # Change Log
+**v.1.0.1 [APPS-Calculator] Release Notes**
+> Re-organized code in PHP
+
+
+> This is beneficial since it makes it easier to maintain, 
+> change, and add new features to the calculator web app.  
 
 **v.1.0 [APPS-Calculator] Release Notes**
-* Imported Smith-Pad-APPS Calculator from the main branch. 
-* Updated README.md for Smith-Pad APPS-Calculator Branch
+> Imported Smith-Pad-APPS Calculator from the main branch. 
+
+> Updated README.md for Smith-Pad APPS-Calculator Branch
---
+++ System_Files/system.global.app.include.ui.php
@@ -0,0 +1,14 @@
+<!----------------------------------------------------------------
+    system.global.app.include.ui.php
+
+    This file is used to include the UI components to show in 
+    the Calculator App. 
+---------------------------------------------------------------->
+
+
+<link rel="stylesheet" href="../src/layouts.css" type="text/css">
+<link rel="stylesheet" href="../src/Custom_Tags.css" type="text/css">
+<link rel="stylesheet" href="../src/position-text.css" type="text/css">
+<link rel="stylesheet" href="../src/UI.css" type="text/css">
+<link rel="stylesheet" href="../src/FOIL-Bar.css" type="text/css">
+<link rel="stylesheet" href="../src/calculator.css" type="text/css">
