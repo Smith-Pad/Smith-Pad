e21cb8b86691bcd269dd8f1990bc0cd3dc0365d2
senalbulumulle <senal.bulumulle@gmail.com>
2022-06-28 10:45:11 -0400

    Let's see if the Subjects UI Components works this way.....

--- Subjects/System_Files/system.global.include.ui.php
+++ Subjects/System_Files/system.global.include.ui.php
@@ -6,16 +6,6 @@
 ---------------------------------------------------------------->
 
 
-<link rel="stylesheet" href="../src/layouts.css" type="text/css">
-<link rel="stylesheet" href="../src/Custom_Tags.css" type="text/css">
-<link rel="stylesheet" href="./src/position-text.css" type="text/css">
-<link rel="stylesheet" href="../src/UI.css" type="text/css">
-<link rel="stylesheet" href="../src/FOIL-Bar.css" type="text/css">
+<link rel="stylesheet" href="../../src/index.css" type="text/css">
+<link rel="stylesheet" href="../../src/UI.css" type="text/css">
 
-
-
-
-
-<link rel="stylesheet" href="../src/FOIL-UI.css" type="text/css">
-<link rel="stylesheet" href="../src/index.css" type="text/css">
-<link rel="stylesheet" href="../src/config.css" type="text/css">
