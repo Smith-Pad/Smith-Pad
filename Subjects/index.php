42353bf3909547b80a0290e69cca7f3838026702
senalbulumulle <senal.bulumulle@gmail.com>
2022-06-28 11:34:52 -0400

    Nowww it is working....

--- Subjects/index.php
+++ Subjects/index.php
@@ -40,7 +40,7 @@ include 'System_Files/system.subjects.header.bar.php';                          
 $MAIN_MENU_SUBJECTS_LIST_Math = "<a href='/Math/Math-Menu.php' class ='FOIL-button'>Math</a>";                                           
 $MAIN_MENU_SUBJECTS_LIST_Reading = "<a href='/Reading/Reading-Menu.php' class ='FOIL-button'>Reading</a>";
 $MAIN_MENU_SUBJECTS_LIST_Science = "<a href='/Science/Science-Menu.php' class='FOIL-button'>Science</a>";
-$MAIN_MENU_SUBJECTS_LIST_BACK_BUTTON = "<a href='../index.php' class='FOIL-button'>Back</a>"; 
+$MAIN_MENU_SUBJECTS_LIST_BACK_BUTTON = print("<a href='../index.php' class='FOIL-button'>Back</a>"); 
 
 
                                         
