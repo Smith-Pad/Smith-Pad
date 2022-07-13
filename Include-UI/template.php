5464030fc7f775e62e2dfe8326f0a89d7d90cb36
senalbulumulle <senal.bulumulle@gmail.com>
2022-07-13 17:35:16 -0400

    okay okay...

--- Include-UI/template.php
+++ Include-UI/template.php
@@ -3,6 +3,10 @@ function include_UI() {
 	print('<link rel="stylesheet" href="./src/layouts.css" type="text/css" />');
 	print('<link rel="stylesheet" href="./src/position-text.css" type="text/css" />');
 	print('<link rel="stylesheet" href="./src/UI.css" type="text/css" />');
+	print('<link rel="stylesheet" href="./src/config.css" type="text/css" />');
+	print('<link rel="stylesheet" href="./src/FOIL-UI.css" type="text/css" />');
+	print('<link rel="stylesheet" href="./src/FOIL-Bar.css" type="text/css" />');
+	print('<link rel="stylesheet" href="./src/animation.css" type="text/css" />');
 	print('<link rel="stylesheet" href="./index.css" type="text/css" />');
 	print('<link rel="stylesheet" href="./background.css" type="text/css" />');
 }
