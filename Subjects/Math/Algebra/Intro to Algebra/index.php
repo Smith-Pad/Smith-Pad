fcffc7117b8c3824bc1282e705300645ce57848b
senalbulumulle <senal.bulumulle@gmail.com>
2022-07-14 15:05:27 -0400

    Re-write Intro to Algebra part-1

--- Subjects/Math/Algebra/Intro to Algebra/index.php
+++ Subjects/Math/Algebra/Intro to Algebra/index.php
@@ -1,21 +1,95 @@
-<?php 
-
-function includeUI() {
-	include './Include-UI/index.php';
-}
+<?php
 
 
-function customizeUI() {
-	include './Customize-UI/index.php';
-}
-
-
-function navigationButtons() {
-	include './Navigation-Buttons/index.php';
-}
-
-
-includeUI();
-customizeUI();
-navigationButtons();
 ?>
+
+
+<link rel="stylesheet" href="./src/layouts.css" type="text/css">
+<link rel="stylesheet" href="./src/position-text.css" type="text/css">
+<link rel="stylesheet" href="./src/UI.css" type="text/css">
+<link rel="stylesheet" href="./index.css" type="text/css">
+<link rel="stylesheet" href="./background.css" type="text/css">
+<style>
+  @import './background.css';
+
+  body {
+    overflow: none;
+
+    color: white;
+    background: var(--main-color-17);
+  }
+</style>
+<div class="bar">
+
+  <a href="../../Math-Menu.php" class="button small button is-red button-no-animate-mode">Home &#9750;</a>
+  <a href="1.php" class="button small button is-red button-no-animate-mode">Next &rArr; </a>
+  <a href="#" class="button"onClick="Calculator=window.open( '../../../../APPS/calculator.php','Calculator', 'width=600,height=1000'); return false;">Launch Calculator</a>
+
+  
+</div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+
+<div class="indicator is-off">
+  <fontsize50>
+    <p align="right">Intro to Algebra </p>
+  </fontsize50>
+
+</div>
+
+<div class="indicator is-off">
+  <a href="1.php" class="button">Intro to Algebra</a>
+  <a href="2.php" class="button">2</a>
+  <a href="3.php" class="button">3</a>
+  <a href="4.php" class="button">4</a>
+  <a href="5.php" class="button">5</a>
+
+
+
+</div>
+
+<ul></ul>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+<div class="space"></div>
+
+</div>
