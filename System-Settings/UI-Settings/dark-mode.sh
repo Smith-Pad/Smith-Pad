25a36226b8274093e2c5c1011b25ecaa820346f7
senalbulumulle <senal.bulumulle@gmail.com>
2022-07-21 13:06:42 -0400

    Made the night mode color schemes a little bit darker

--- System-Settings/UI-Settings/dark-mode.sh
+++ System-Settings/UI-Settings/dark-mode.sh
@@ -21,24 +21,24 @@ echo """
 @import url('FOIL-UI.css');
 
 body {
-        background: linear-gradient(120deg, rgb(29, 0, 66) 51%, rgb(15, 39, 0) 70%);
+        background: linear-gradient(120deg, rgb(15, 1, 32) 51%, rgb(9, 24, 0) 70%);
         color: white;
 }
 
 .FOIL-button {
-        background: linear-gradient(120deg, rgb(35, 0, 82) 51%, rgba(41, 102, 0));
+        background: linear-gradient(120deg, rgb(15, 1, 32) 51%, rgb(9, 24, 0) 70%);
 }
 
 
 .FOIL-button:hover {
-        background: linear-gradient(120deg, rgba(134, 44, 255, 1) 51%, rgba(110, 148, 84, 1) 70%);
+        background: linear-gradient(120deg, rgb(45, 0, 104) 51%, rgba(110, 148, 84, 1) 70%);
         box-shadow: red;
         border-style: solid;
         padding: 100px 43px;
 }
 
 .FOIL-button-small {
-        background: linear-gradient(90deg, rgba(135, 44, 255, 0.082) 21%, rgba(110, 148, 84, 0.087) 58%);
+        background: linear-gradient(120deg, rgb(15, 1, 32) 51%, rgb(9, 24, 0) 70%);
         font-size: 20px;
         border-top: 1px;
         border-color: rgba(110, 148, 84, 1) rgba(134, 44, 255, 1);
@@ -51,14 +51,14 @@ body {
 
 
 .FOIL-Navigation-Bar {
-        background: linear-gradient(circle, rgba(110, 148, 84, 1) 0%, rgba(134, 44, 255, 1) 80%);
+        background-color: transparent;
         border-radius: 10px;
         color: white;
 }
 
 
 .FOIL-Layout {
-        background: linear-gradient(90deg, rgba(135, 44, 255, 0.082) 21%, rgba(110, 148, 84, 0.087) 58%);
+        background-color: transparent;
         border-radius: 10px;
         color: white;
 }
