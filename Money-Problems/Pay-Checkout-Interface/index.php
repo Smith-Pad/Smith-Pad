2802707dfd95d316cbd50b0f7c721c0bbac2217c
senalbulumulle <senal.bulumulle@gmail.com>
2022-09-25 18:13:10 -0400

    Formatting code is the best...

--- Money-Problems/Pay-Checkout-Interface/index.php
+++ Money-Problems/Pay-Checkout-Interface/index.php
@@ -63,7 +63,8 @@
 		print('<div class="FOIL-layout-regular-no-shadow">');
 		print('<FOIL-font-size-31> <center> Welcome to the Lesson </center> </FOIL-font-size-31>');
 		print('<div class="FOIL-layout-regular-no-shadow">');
-		print('<center> <b> Welcome to this interactive lesson where you will explore a grocery store and learn how to use the pay checkout </b></center>');
+		print('<center> <b> Welcome to this interactive lesson where you will explore a grocery store 
+		       and learn how to use the pay checkout </b></center>');
 
 		print('<div class="FOIL-layout-regular-no-shadow">');
 		print('<div class="FOIL-layout-regular-no-shadow">');
