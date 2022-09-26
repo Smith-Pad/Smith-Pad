b45a7ab55f1a911731aa17cc5936c2beddbf5b76
senalbulumulle <senal.bulumulle@gmail.com>
2022-09-26 09:42:02 -0400

    Updated

--- Money-Problems/Pay-Checkout-Interface/2.php
+++ Money-Problems/Pay-Checkout-Interface/2.php
@@ -40,7 +40,7 @@
 	function naviButtons() {
 		print('<hr>');
 		print('<a href="index.php" class="FOIL-button-small-no-shadow">Home</a>');
-		print('<a href="index.php" class="FOIL-button-small-no-shadow">Previous</a>');
+		print('<a href="1.php" class="FOIL-button-small-no-shadow">Previous</a>');
 		print('<a href="2.php" class="FOIL-button-small-no-shadow">Next</a>');
 		print('</div>');
 	}
@@ -57,10 +57,44 @@
 	 */
 
 	function content() {
-                print('<div class="FOIL-layout-regular">');
-                print('<FOIL-font-size-34> What is a Pay Checkout? </FOIL-font-size-34>');
-                print('description later...');
-                print('</div>');
+                // print('<div class="character-photo">');
+
+		// Question Banner
+		print('
+			<div class="question-banner">
+			<FOIL-font-size-31> <center> CHARACTER PHOTO </center> </FOIL-font-size-31>
+			<center> <b> Senal is buying 2 mixed vegetables to make his rice and curry. </b></center>
+			<center> <b> There are both $55.00 </b></center>
+			<center> <b> How much is the total? </b></center>
+			</div>
+		');
+
+		// Develop a table
+		print('
+			<div class="FOIL-layout-regular">
+			<center>
+				<table>
+					<thead>
+						<tr>
+							<th>Food</th>
+							<th>Price</th>
+						</tr>
+					</thead>
+                                        <tbody> 
+					
+						<tr>
+							<td>&nbsp; Mixed Vegetables</td>
+							<td>&nbsp; $55.00</td>
+						</tr>
+
+						<tr>
+                                                        <td>&nbsp; Mixed Vegetables</td>
+                                                        <td>&nbsp; $55.00</td>
+						</tr>
+					</tbody>
+				</table>
+			</center>
+		');
 	}
 
 	beginFontStyles();
