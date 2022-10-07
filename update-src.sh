d8fcbab081b9df9f959341011ed21c481d1970cf
senalbulumulle <senal.bulumulle@gmail.com>
2022-10-07 14:49:46 -0400

    Created Script to be more efficient when updating the src files.

---
+++ update-src.sh
@@ -0,0 +1,5 @@
+## update-src.sh
+## Script to update [src] efficiently to the Folders 
+## Making it easier to update it. Without having to 
+## Manually update all of the src files for a long time. 
+
