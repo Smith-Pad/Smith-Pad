c171d0d7897ceb6663d3f5d8f424f68c07d0a8c2
senalbulumulle <senal.bulumulle@gmail.com>
2022-10-07 17:51:25 -0400

    The update-src.sh works! yay!

--- update-src.sh
+++ update-src.sh
@@ -3,3 +3,31 @@
 ## Making it easier to update it. Without having to 
 ## Manually update all of the src files for a long time. 
 
+function copyToAppsFolder() {
+        cp -R src Applications
+        echo "[ OK ]"
+}
+
+
+function copyToHomeScreenFolder() {
+        cp -R src Home-Screen
+        echo "[ OK ]"
+}
+
+function copyToSettingsMenu() {
+        cp -R src Settings-Menu
+        echo "[ OK ]"
+}
+
+
+function copytoSubjects() {
+        cp -R src Subjects 
+        echo "[ OK ]"
+}
+
+
+
+copyToAppsFolder
+copyToHomeScreenFolder
+copyToSettingsMenu
+copytoSubjects
