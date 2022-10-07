dd1e9786d95566f36a9f16366641ef8a14e13cef
senalbulumulle <senal.bulumulle@gmail.com>
2022-10-07 14:36:12 -0400

    Allow the script the ability to create the directories in the Home Screen in [/var/]

--- compile.sh
+++ compile.sh
@@ -3,3 +3,29 @@
 ## Compile things to make it work
 
 
+## Function 
+
+function enableSuperUser() {
+        clear
+        sudo su
+}
+
+## Make a function to create a directory in 
+function createDirectory() {
+        ## Go to the /var/ directory
+        cd /var/
+
+        ## ------------HOME SCREEN --------------------
+        ##| This will create directories first in the 
+        ##| Home Screen. 
+
+        ## Create the directories in the Home Screen
+        mkdir System_Files/
+        mkdir src
+        mkdir APPS
+        mkdir Settings-Menu
+}
+
+
+enableSuperUser
+createDirectory
