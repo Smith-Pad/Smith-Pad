b4df0fe23d4601ed0a1afef07bfbf820d1f54535
senalbulumulle <senal.bulumulle@gmail.com>
2022-10-08 16:10:28 -0400

    I think it is best not to generate this and that for now..

--- compile.sh
+++ compile.sh
@@ -3,18 +3,7 @@
 ## Compile things to make it work
 
 function createDirectory() {
-        ## Go to the /var/ directory
-        cd /var
 
-        ## ------------HOME SCREEN --------------------
-        ##| This will create directories first in the 
-        ##| Home Screen. 
-
-        ## Create the directories in the Home Screen
-        mkdir System_Files/
-        mkdir src
-        mkdir APPS
-        mkdir Settings-Menu
 }
 
 createDirectory
