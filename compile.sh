3102acddf33015f6e7a95ac3569c991475a6b1f7
senalbulumulle <senal.bulumulle@gmail.com>
2022-10-08 15:45:59 -0400

    Working on the compile script part 2

--- compile.sh
+++ compile.sh
@@ -3,29 +3,6 @@
 ## Compile things to make it work
 
 
-## Function 
+cd /var
 
-function enableSuperUser() {
-        clear
-        sudo su
-}
-
-## Make a function to create a directory in 
-function createDirectory() {
-        ## Go to the /var/ directory
-        cd /var
-
-        ## ------------HOME SCREEN --------------------
-        ##| This will create directories first in the 
-        ##| Home Screen. 
-
-        ## Create the directories in the Home Screen
-        mkdir System_Files/
-        mkdir src
-        mkdir APPS
-        mkdir Settings-Menu
-}
-
-
-enableSuperUser
-createDirectory
+mkdir HELLO
