ef5dd0c0a4412a28240a248791f3f63c934aedb4
senalbulumulle <senal.bulumulle@gmail.com>
2022-10-09 12:37:47 -0400

    Testing the ability to suspend the system via button press part-1

--- EXPORT-TO-COMPILE-TESTS/System_Files/system.global.include.navi.buttons.php
+++ EXPORT-TO-COMPILE-TESTS/System_Files/system.global.include.navi.buttons.php
@@ -5,7 +5,7 @@
 function naviButtons() {
         print('<a href="#" class="FOIL-button-regular-no-shadow">Shutdown</a>');
         print('<a href="#" class="FOIL-button-regular-no-shadow">Restart</a>');
-        print('<a href="#" class="FOIL-button-regular-no-shadow">Suspend</a>');
+        print('<a href="system.global.include.sleep.mode.php" class="FOIL-button-regular-no-shadow">Suspend</a>');
         print('</div>');
 }
 naviButtons();
---
+++ EXPORT-TO-COMPILE-TESTS/System_Files/system.global.include.sleep.mode.php
@@ -0,0 +1,4 @@
+<?php
+        exec('systemctl suspend');
+
+?>
