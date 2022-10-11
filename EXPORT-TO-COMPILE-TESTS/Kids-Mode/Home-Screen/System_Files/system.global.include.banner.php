8f6a7e3a1bd3af92d7fee1d028177ff6e357f75d
senalbulumulle <senal.bulumulle@gmail.com>
2022-10-11 12:42:38 -0400

    Updated: EXPORT-TO-COMPILE-TESTS

--- EXPORT-TO-COMPILE-TESTS/Kids-Mode/Home-Screen/System_Files/system.global.include.banner.php
+++ EXPORT-TO-COMPILE-TESTS/Kids-Mode/Home-Screen/System_Files/system.global.include.banner.php
@@ -2,7 +2,7 @@
 function banner() {
         print('<div class="FOIL-font-styles sans-serif">');
         print('<div class="FOIL-navigation-bar-regular">');
-        print('<h1> Smith Pad </h1>');
+        print('<h1> Smith Pad<FOIL-font-size-10> KIDS MODE </FOIL-font-size-10> </h1>');
 }
 banner();
 ?>
