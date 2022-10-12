653c372d06954522f48fd9131dadbcf0c2f8f746
senalbulumulle <senal.bulumulle@gmail.com>
2022-10-12 11:02:34 -0400

    Updated [EXPORT-TO-COMPILE-TESTS.sh]

--- EXPORT-TO-COMPILE-TESTS/Kids-Mode/Settings-Menu/System_Files/system.global.include.navi.buttons.php
+++ EXPORT-TO-COMPILE-TESTS/Kids-Mode/Settings-Menu/System_Files/system.global.include.navi.buttons.php
@@ -3,9 +3,9 @@
 // Example
 // <a href="#" class="FOIL-button-regular-no-shadow"></a>
 function naviButtons() {
-        print('<a href="#" class="FOIL-button-regular-no-shadow">Shutdown</a>');
-        print('<a href="#" class="FOIL-button-regular-no-shadow">Restart</a>');
-        print('<a href="#" class="FOIL-button-regular-no-shadow">Suspend</a>');
+        //print('<a href="#" class="FOIL-button-regular-no-shadow">Shutdown</a>');
+        //print('<a href="#" class="FOIL-button-regular-no-shadow">Restart</a>');
+        //print('<a href="#" class="FOIL-button-regular-no-shadow">Suspend</a>');
         print('</div>');
 }
 naviButtons();
