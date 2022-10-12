f742f82b449531b4acd3e2fafa707ca8432a4c9a
senalbulumulle <senal.bulumulle@gmail.com>
2022-10-12 11:01:28 -0400

    Disabled the ability to Shutdown, Restart, or Suspend Device in the Interface for Kids Mode

--- Kids-Mode/Subjects/System_Files/system.global.include.navi.buttons.php
+++ Kids-Mode/Subjects/System_Files/system.global.include.navi.buttons.php
@@ -3,9 +3,9 @@
 // Example
 // <a href="#" class="FOIL-button-regular-no-shadow"></a>
 function naviButtons() {
-        print('<a href="#" class="FOIL-button-small-no-shadow">Shutdown</a>');
-        print('<a href="#" class="FOIL-button-small-no-shadow">Restart</a>');
-        print('<a href="#" class="FOIL-button-small-no-shadow">Suspend</a>');
+        //print('<a href="#" class="FOIL-button-small-no-shadow">Shutdown</a>');
+        //print('<a href="#" class="FOIL-button-small-no-shadow">Restart</a>');
+        //print('<a href="#" class="FOIL-button-small-no-shadow">Suspend</a>');
         print('</div>');
 }
 naviButtons();
