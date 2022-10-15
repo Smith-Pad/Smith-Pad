f9ad46a5fef661f7bc4be451ecfd7814bf564688
senalbulumulle <senal.bulumulle@gmail.com>
2022-10-15 10:02:06 -0400

    Changed for the for loop for the number of layouts stacked each other... [Settings-Menu] [Kids Mode]

--- Kids-Mode/Settings-Menu/System_Files/system.global.include.content.php
+++ Kids-Mode/Settings-Menu/System_Files/system.global.include.content.php
@@ -15,16 +15,12 @@ function content() {
                 print('<div class="FOIL-layout-regular-no-shadow">');   
         }
 
-
+        
+        print('<div class="FOIL-layout-regular-no-shadow">');
         print('<center> <FOIL-font-size-60> <div id="time_service"></div> </FOIL-font-size-60> </center>');
         print('<script src="src/FOIL-time.js"></script>');
-        print('<center><FOIL-font-size-40> Settings Menu </FOIL-font-size-40></center>');
-        print('<a href="../index.php" class="FOIL-button-regular-no-shadow">Home</a>');
-        print('<a href="Bluetooth/index.php" class="FOIL-button-regular-no-shadow">Bluetooth</a>');
-        print('<a href="Wi-Fi/index.php" class="FOIL-button-regular-no-shadow">Wi-Fi</a>');
-        print('<a href="Language/index.php" class="FOIL-button-regular-no-shadow">Language</a>');
-        print('<a href="Display/index.php" class="FOIL-button-regular-no-shadow">Display</a>');
-        print('<a href="About/index.php" class="FOIL-button-regular-no-shadow">About</a>');
+        print('<center><FOIL-font-size-40> About </FOIL-font-size-40></center>');
+        include('Content/story.content/story.content');
 }
 content();
 ?>
