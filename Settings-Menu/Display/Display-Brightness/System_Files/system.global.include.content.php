f77152812899ed7b69c01601daa5ec85e992217c
senalbulumulle <senal.bulumulle@gmail.com>
2022-10-18 14:02:33 -0400

    Ohhh boy... I have an idea... with xdotool

---
+++ Settings-Menu/Display/Display-Brightness/System_Files/system.global.include.content.php
@@ -0,0 +1,26 @@
+<?php
+function content() {
+
+        /** 
+         * For-loop. 
+         * 
+         * 
+         * 
+         * How to adjust the number of layouts 
+         * 
+         * On the [$layout <= NUMBER OF LAYOUTS], that's where you provide 
+         * the number of layouts to adjust. 
+         */
+        for ($layout = 0; $layout <= 5; $layout +=10) {
+                print('<div class="FOIL-layout-regular-no-shadow">');   
+        }
+
+
+        print('<center> <FOIL-font-size-60> <div id="time_service"></div> </FOIL-font-size-60> </center>');
+        print('<script src="src/FOIL-time.js"></script>');
+        print('<center><FOIL-font-size-40> Display Brightness </FOIL-font-size-40></center>');
+        print('<center><a href="../low.php" class="FOIL-button-regular-no-shadow">Low</a></center>');
+        print('<center><a href="../high.php" class="FOIL-button-regular-no-shadow">High</a></center>');
+}
+content();
+?>
---
+++ Settings-Menu/Display/Display-Brightness/low.php
@@ -0,0 +1,5 @@
+<?php 
+
+exec('xdotool key XF86 Mon BrightnessUp ');
+
+?>
