79bcccb619783d6bd69cd5f7c9ea8aaae9d82518
senalbulumulle <senal.bulumulle@gmail.com>
2022-10-18 12:27:57 -0400

    mmmm...

--- Settings-Menu/Display/System_Files/system.global.include.content.php
+++ Settings-Menu/Display/System_Files/system.global.include.content.php
@@ -1,31 +1,28 @@
 <?php
 function content() {
-        print('<div class="FOIL-layout-regular-no-shadow">');
-        print('<div class="FOIL-layout-regular-no-shadow">');
-        print('<div class="FOIL-layout-regular-no-shadow">');
-        print('<div class="FOIL-layout-regular-no-shadow">');
-        print('<div class="FOIL-layout-regular-no-shadow">');
+
+        /** 
+         * For-loop. 
+         * 
+         * 
+         * 
+         * How to adjust the number of layouts 
+         * 
+         * On the [$layout <= NUMBER OF LAYOUTS], that's where you provide 
+         * the number of layouts to adjust. 
+         */
+        for ($layout = 0; $layout <= 5; $layout +=10) {
+                print('<div class="FOIL-layout-regular-no-shadow">');   
+        }
+
+
         print('<center> <FOIL-font-size-60> <div id="time_service"></div> </FOIL-font-size-60> </center>');
         print('<script src="src/FOIL-time.js"></script>');
-        print('</div>');
-        print('<center><FOIL-font-size-40> Display </FOIL-font-size-40></center>');
-        print('</div>');
-        print('</div>');
-        print('<a href="../index.php" class="FOIL-button-regular-no-shadow">Home</a>');
-        // If the user presses display resolution, user will be able to select settings for 
-        // display resolution.
+        print('<center><FOIL-font-size-40> Display Settings </FOIL-font-size-40></center>');
 
 
-        print('<a href="Display-Resolution/index.php" class="FOIL-button-regular-no-shadow">Display-Resolution</a>');
-        // If the user presses dark mode, user will able to see dark mode colors on UI
-        print('<a href="dark-mode.php" class="FOIL-button-regular-no-shadow">UI TO DARK COLORS</a>');
-
-        // If the user presses display brightness, user will be able to select display brightness
-        //print('<a href="Display-Brightness/index.php" class="FOIL-button-regular-no-shadow">Display Brightness</a>');
-        print('<a href="Display-Brightness/index.php" class="FOIL-button-regular-no-shadow">Display Brightness[EXTERNAL]</a>');
-
-        print('</div>');
-        print('</div>');
+        // After the user presses this button, it will allow the user to adjust the display brightness.
+        print('<a href="Display-Brightness/index.php" class="FOIL-button-regular-no-shadow">Display Brightness</a>');
 }
 content();
 ?>
