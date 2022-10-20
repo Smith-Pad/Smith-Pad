0289907e160a81169368acd29ae27740563f894a
senalbulumulle <senal.bulumulle@gmail.com>
2022-10-20 11:12:37 -0400

    Organizing the branches. This branch is now used for documentation.

--- APPS/APPS-Calculator/System_Files/system.app.calculator.top.name.php
+++
@@ -1,6 +0,0 @@
-<!----------------------------------------------------------------
-    system.app.calculator.top.name.php
-
-    This file is used to show the word "Calculator" top center
----------------------------------------------------------------->
-<center> <h1> Calculator</h1></center>
