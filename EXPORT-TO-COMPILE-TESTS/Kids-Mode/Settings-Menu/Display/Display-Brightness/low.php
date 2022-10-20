1f0ac53efb14d5c25f7e2c6d9fa3844788a173fd
senalbulumulle <senal.bulumulle@gmail.com>
2022-10-20 12:52:41 -0400

    Cleanup

--- EXPORT-TO-COMPILE-TESTS/Kids-Mode/Settings-Menu/Display/Display-Brightness/low.php
+++
@@ -1,6 +0,0 @@
-<meta http-equiv="Refresh" content="0; URL=index.php" />
-<?php 
-exec('xdotool key XF86MonBrightnessDown');       
-exec('xdotool key XF86MonBrightnessDown');   
-exec('xdotool key XF86MonBrightnessDown');       
-?>
