c2646eb4d92d56ccefd68f97788ef13bf96dd1f2
senalbulumulle <senal.bulumulle@gmail.com>
2022-10-20 12:45:18 -0400

    Cleaning up...

--- EXPORT-TO-COMPILE-TESTS/Settings-Menu/Display/Display-Brightness/high.php
+++
@@ -1,6 +0,0 @@
-<meta http-equiv="Refresh" content="0; URL=index.php" />
-<?php 
-exec('xdotool key XF86MonBrightnessUp');       
-exec('xdotool key XF86MonBrightnessUp');       
-exec('xdotool key XF86MonBrightnessUp');       
-?>
