c1d5e5eeacefde0e1269a912dbe6443f744b4e27
senalbulumulle <senal.bulumulle@gmail.com>
2022-10-20 13:03:11 -0400

    Updated

---
+++ Applications/System_Files/system.global.include.content.php
@@ -0,0 +1,23 @@
+<?php
+function content() {
+        print('<div class="FOIL-layout-regular-no-shadow">');
+        print('<div class="FOIL-layout-regular-no-shadow">');
+        print('<div class="FOIL-layout-regular-no-shadow">');
+        print('<div class="FOIL-layout-regular-no-shadow">');
+        print('<div class="FOIL-layout-regular-no-shadow">');
+        print('<center> <FOIL-font-size-60> <div id="time_service"></div> </FOIL-font-size-60> </center>');
+        print('<script src="src/FOIL-time.js"></script>');
+        print('</div>');
+        print('<center><FOIL-font-size-40> Menu </FOIL-font-size-40></center>');
+        print('</div>');
+        print('</div>');
+        print('<a href="#" class="FOIL-button-regular-no-shadow">Settings</a>');
+        print('<a href="#" class="FOIL-button-regular-no-shadow">Schedule Display</a>');
+        print('<a href="#" class="FOIL-button-regular-no-shadow">Math Operations</a>');
+        print('<a href="#" class="FOIL-button-regular-no-shadow">Subjects</a>');
+        print('<a href="../clock-mode.php" class="FOIL-button-regular-no-shadow">Clock Mode</a>');
+        print('</div>');
+        print('</div>');
+}
+content();
+?>
