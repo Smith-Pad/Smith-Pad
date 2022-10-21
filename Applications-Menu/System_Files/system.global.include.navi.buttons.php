e6fe6e6035833b33e62c9bb405554633bfea40b3
senalbulumulle <senal.bulumulle@gmail.com>
2022-10-21 10:43:11 -0400

    Added Applications Menu

---
+++ Applications-Menu/System_Files/system.global.include.navi.buttons.php
@@ -0,0 +1,13 @@
+<?php
+// Include Buttons in navigation if it is required so
+// Example
+// <a href="#" class="FOIL-button-regular-no-shadow"></a>
+function naviButtons() {
+        print('<a href="#" class="FOIL-button-regular-no-shadow">Shutdown</a>');
+        print('<a href="#" class="FOIL-button-regular-no-shadow">Restart</a>');
+        print('<a href="system.global.include.sleep.mode.php" class="FOIL-button-regular-no-shadow">Suspend</a>');
+        print('<a href="../index.php" class="FOIL-button-regular-no-shadow">Back</a>');
+        print('</div>');
+}
+naviButtons();
+?>
