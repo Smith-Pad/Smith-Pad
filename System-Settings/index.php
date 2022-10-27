2f1357c194b91c965dfbbe43207d059efe41e085
senalbulumulle <senal.bulumulle@gmail.com>
2022-10-27 14:37:03 -0400

    Updated UI Elements in System-Settings to match the new interface but it is the same layout

--- System-Settings/index.php
+++ System-Settings/index.php
@@ -8,20 +8,15 @@
 <?php 
 
 function includeUI() {
-        print('<link rel="stylesheet" href="/../src/layouts.css" type="text/css">');
-        print('<link rel="stylesheet" href="../../src/Custom_Tags.css" type="text/css">');
-        print('<link rel="stylesheet" href="../../src/position-text.css" type="text/css">');
-        print('<link rel="stylesheet" href="../../src/UI.css" type="text/css">');
-        print('<link rel="stylesheet" href="../../src/FOIL-Bar.css" type="text/css">');
-        print('<link rel="stylesheet" href="../../src/FOIL-UI.css" type="text/css">'); 
-        print('<link rel="stylesheet" href="../../src/config.css" type="text/css">');
-        print('<link rel="stylesheet" href="../../src/animation.css" type="text/css">');
+        print('<link rel="stylesheet" href="./src/FOIL-UI.css" type="text/css">');
+        print('<link rel="stylesheet" href="./src/config.css" type="text/css">');
 }
 
 function banner() {
+        print('<div class="FOIL-font-styles sans-serif">');
         print('<div class="FOIL-navigation-bar-regular">');
-        print('<h1> System Settings </h1>');                                                            // Banner Name
-        print('<a href="../index.php" class="FOIL-button-small">Back</a>');                             // Go back to the Home Menu
+        print('<h1> System Settings </h1>');               
+        print('</div>');                                                                                                        // Banner Name
         print('</div>');
 }
 
@@ -33,31 +28,33 @@ function banner() {
  */
 
 function displayOptions() {
+        print('<div class="FOIL-font-styles sans-serif">');
         print('<div class="FOIL-layout-regular">');
         print('<FOIL-Font-Size-60>Display Options</FOIL-Font-Size-60>');
         include('../src/avakasaya/space4.php');
         print('<FOIL-Font-Size-30>Rotation</FOIL-Font-Size-30>');
         include('../src/avakasaya/space2.php');
-        print('<a href="Screen-Resolution/normal.php" class="FOIL-button-regular">Normal</a>');                 // Make the screen normal
-        print('<a href="Screen-Resolution/inverted.php" class="FOIL-button-regular">Flipped</a>');              // Make the screen flipped
-        print('<a href="Screen-Resolution/left.php" class="FOIL-button-regular">Portrait (Left)</a>');          // Make the screen portrait
-        print('<a href="Screen-Resolution/right.php" class="FOIL-button-regular">Portrait (Right)</a>');        // Make the screen portrait        
+        print('<a href="Screen-Resolution/normal.php" class="FOIL-button-regular-no-shadow">Normal</a>');                       // Make the screen normal
+        print('<a href="Screen-Resolution/inverted.php" class="FOIL-button-regular-no-shadow">Flipped</a>');                    // Make the screen flipped
+        print('<a href="Screen-Resolution/left.php" class="FOIL-button-regular-no-shadow">Portrait (Left)</a>');                // Make the screen portrait
+        print('<a href="Screen-Resolution/right.php" class="FOIL-button-regular-no-shadow">Portrait (Right)</a>');              // Make the screen portrait        
         include('../src/avakasaya/space4.php');
         print('<FOIL-Font-Size-30>Display Brightness</FOIL-Font-Size-30>');
         include('../src/avakasaya/space2.php');
-        print('<a href="Display-Brightness/10-percent.php" class="FOIL-button-regular">10</a>');		// Set Display Brightness to 10%
-        print('<a href="Display-Brightness/20-percent.php" class="FOIL-button-regular">20</a>');		// Set Display Brightness to 20%
-        print('<a href="Display-Brightness/30-percent.php" class="FOIL-button-regular">30</a>');		// Set Display Brightness to 30%
+        print('<a href="Display-Brightness/10-percent.php" class="FOIL-button-regular-no-shadow">10</a>');		        // Set Display Brightness to 10%
+        print('<a href="Display-Brightness/20-percent.php" class="FOIL-button-regular-no-shadow">20</a>');		        // Set Display Brightness to 20%
+        print('<a href="Display-Brightness/30-percent.php" class="FOIL-button-regular-no-shadow">30</a>');		        // Set Display Brightness to 30%
         include('../src/avakasaya/space2.php');
-        print('<a href="Display-Brightness/40-percent.php" class="FOIL-button-regular">40</a>');		// Set Display Brightness to 40%
-        print('<a href="Display-Brightness/50-percent.php" class="FOIL-button-regular">50</a>');		// Set Display Brightness to 50%
-        print('<a href="Display-Brightness/60-percent.php" class="FOIL-button-regular">60</a>');		// Set Display Brightness to 60%
+        print('<a href="Display-Brightness/40-percent.php" class="FOIL-button-regular-no-shadow">40</a>');		        // Set Display Brightness to 40%
+        print('<a href="Display-Brightness/50-percent.php" class="FOIL-button-regular-no-shadow">50</a>');		        // Set Display Brightness to 50%
+        print('<a href="Display-Brightness/60-percent.php" class="FOIL-button-regular-no-shadow">60</a>');		        // Set Display Brightness to 60%
         include('../src/avakasaya/space2.php');
-        print('<a href="Display-Brightness/70-percent.php" class="FOIL-button-regular">70</a>');		// Set Display Brightness to 70%
-        print('<a href="Display-Brightness/80-percent.php" class="FOIL-button-regular">80</a>');		// Set Display Brightness to 80%
-        print('<a href="Display-Brightness/90-percent.php" class="FOIL-button-regular">90</a>');		// Set Display Brightness to 90%
+        print('<a href="Display-Brightness/70-percent.php" class="FOIL-button-regular-no-shadow">70</a>');		        // Set Display Brightness to 70%
+        print('<a href="Display-Brightness/80-percent.php" class="FOIL-button-regular-no-shadow">80</a>');		        // Set Display Brightness to 80%
+        print('<a href="Display-Brightness/90-percent.php" class="FOIL-button-regular-no-shadow">90</a>');		        // Set Display Brightness to 90%
         include('../src/avakasaya/space2.php');
-        print('<a href="Display-Brightness/100-percent.php" class="FOIL-button-regular">100</a>');		// Set Display Brightness to 100%
+        print('<a href="Display-Brightness/100-percent.php" class="FOIL-button-regular-no-shadow">100</a>');		        // Set Display Brightness to 100%
+        print('</div>');
         print('</div>');
 }
 
@@ -69,11 +66,13 @@ function displayOptions() {
  */
 
 function uiSettings() {
+        print('<div class="FOIL-font-styles sans-serif">');
         print('<div class="FOIL-layout-regular">');
         print('<FOIL-Font-Size-60>UI Settings</FOIL-Font-Size-60>');
         include('../src/avakasaya/space4.php');
-        // print('<a href="UI-Settings/light-mode.php" class="FOIL-button-regular">Light Mode</a>');                // Change to Light Mode
-        // print('<a href="UI-Settings/dark-mode.php" class="FOIL-button-regular">Dark Mode</a>');                  // Change to Dark Mode
+        // print('<a href="UI-Settings/light-mode.php" class="FOIL-button-regular-no-shadow">Light Mode</a>');                  // Change to Light Mode
+        // print('<a href="UI-Settings/dark-mode.php" class="FOIL-button-regular-no-shadow">Dark Mode</a>');                    // Change to Dark Mode
+        print('</div>');
         print('</div>');
 }
 
@@ -99,17 +98,17 @@ function packageManager() {
         include('../src/avakasaya/space4.php');
 
         // Update Packages
-        print('<a href="Package-Management-Scripts/update-packages.php" class="FOIL-button-regular">Update</a>');
+        print('<a href="Package-Management-Scripts/update-packages.php" class="FOIL-button-regular-no-shadow">Update</a>');
 
         // Clear Package Cache
-        print('<a href="Package-Management-Scripts/clear-package-cache.php" class="FOIL-button-regular">Clear Package Cache</a>');   
+        print('<a href="Package-Management-Scripts/clear-package-cache.php" class="FOIL-button-regular-no-shadow">Clear Package Cache</a>');   
         
         // List Packages 
-        print('<a href="Package-Management-Scripts/list-packages.php" class="FOIL-button-regular">List Packages</a>');
+        print('<a href="Package-Management-Scripts/list-packages.php" class="FOIL-button-regular-no-shadow">List Packages</a>');
 
 
         // Update Smith-Pad Web Interface
-        print('<a href="Update-Interface/index.php" class="FOIL-button-regular">Update Shell</a>');
+        print('<a href="Update-Interface/index.php" class="FOIL-button-regular-no-shadow">Update Shell</a>');
 
 
         print('</div>');
