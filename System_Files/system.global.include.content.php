8a3f89dc7285fbd1aefe86b4506362eecac8b514
senalbulumulle <senal.bulumulle@@gmail.com>
2022-11-20 15:41:32 -0500

    Disactivated the ability to go to Apps nor Settings for now...

--- System_Files/system.global.include.content.php
+++ System_Files/system.global.include.content.php
@@ -18,9 +18,9 @@ function content() {
         print('<center> <FOIL-font-size-60> <div id="time_service"></div> </FOIL-font-size-60> </center>');
         print('<script src="src/FOIL-time.js"></script>');
         print('<center><FOIL-font-size-40> Menu </FOIL-font-size-40></center>');
-        print('<a href="System-Settings/index.php" class="FOIL-button-regular-no-shadow">Settings</a>');
+        print('<a href="#" class="FOIL-button-regular-no-shadow">Settings</a>');
         print('<a href="Subjects/index.php" class="FOIL-button-regular-no-shadow">Subjects</a>');
-        print('<a href="Applications-Menu/index.php" class="FOIL-button-regular-no-shadow">Apps</a>');
+        print('<a href="#" class="FOIL-button-regular-no-shadow">Apps</a>');
 }
 content();
 ?>
