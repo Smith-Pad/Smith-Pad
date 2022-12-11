f6548eb84387cfb35145f02b5ba135bbe282de94
senalbulumulle <senal.bulumulle@gmail.com>
2022-12-11 10:26:48 -0500

    Re-designing the web app Calculator...

---
+++ APPS-Calculator/System_Files/system.app.calculator.content.php
@@ -0,0 +1,40 @@
+<!----------------------------------------------------------------
+    system.app.calculator.content.php
+
+    This file is used to show the content.      
+---------------------------------------------------------------->
+
+<div id='result'>
+<center>
+<form name="calculator">
+      
+<input type="text" name="answer" />
+<br>
+      
+<input type="button" value=" 1 " class="FOIL-button-regular-no-shadow" onclick="calculator.answer.value += '1'" />
+<input type="button" value=" 2 " class="FOIL-button-regular-no-shadow" onclick="calculator.answer.value += '2'" />
+<input type="button" value=" 3 " class="button-no-animate-mode"onclick="calculator.answer.value += '3'" />
+<input type="button" value=" + " class="button-no-animate-mode"onclick="calculator.answer.value += '+'" />
+<br/>
+      
+<input type="button" value=" 4 " class="button-no-animate-mode"onclick="calculator.answer.value += '4'" />
+<input type="button" value=" 5 " class="button-no-animate-mode" onclick="calculator.answer.value += '5'" />
+<input type="button" value=" 6 " class="button-no-animate-mode"onclick="calculator.answer.value += '6'" />
+<input type="button" value=" - " class="button-no-animate-mode"onclick="calculator.answer.value += '-'" />
+</br>
+    
+<input type="button" value=" 7 " class="button-no-animate-mode"onclick="calculator.answer.value += '7'" />
+<input type="button" value=" 8 "  class="button-no-animate-mode" onclick="calculator.answer.value += '8'" />
+<input type="button" value=" 9 "  class="button-no-animate-mode"onclick="calculator.answer.value += '9'" />
+<input type="button" value=" x "  class="button-no-animate-mode"onclick="calculator.answer.value += '*'" />
+</br>
+  
+<input type="button" value=" c "  class="button-no-animate-mode"onclick="calculator.answer.value = ''" />
+<input type="button" value=" 0 "  class="button-no-animate-mode"onclick="calculator.answer.value += '0'" />
+<input type="button" value=" = "  class="button-no-animate-mode"onclick="calculator.answer.value = eval(calculator.answer.value)" />
+<input type="button" value=" / "  class="button-no-animate-mode"onclick="calculator.answer.value += '/'" />
+</br>
+
+
+</form>
+</center>
---
+++ APPS-Calculator/System_Files/system.app.calculator.top.name.php
@@ -0,0 +1,6 @@
+<!----------------------------------------------------------------
+    system.app.calculator.top.name.php
+
+    This file is used to show the word "Calculator" top center
+---------------------------------------------------------------->
+<center> <h1> Calculator</h1></center>
