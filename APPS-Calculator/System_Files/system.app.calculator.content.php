39af25075000b8a5be75455d47c61ac254b83a1a
senalbulumulle <senal.bulumulle@gmail.com>
2022-12-11 10:44:38 -0500

    Replaced the original Smith-Pad UI Syntax to FOIL-UI syntax in calculator

--- APPS-Calculator/System_Files/system.app.calculator.content.php
+++ APPS-Calculator/System_Files/system.app.calculator.content.php
@@ -13,26 +13,26 @@
       
 <input type="button" value=" 1 " class="FOIL-button-regular-no-shadow" onclick="calculator.answer.value += '1'" />
 <input type="button" value=" 2 " class="FOIL-button-regular-no-shadow" onclick="calculator.answer.value += '2'" />
-<input type="button" value=" 3 " class="button-no-animate-mode"onclick="calculator.answer.value += '3'" />
-<input type="button" value=" + " class="button-no-animate-mode"onclick="calculator.answer.value += '+'" />
+<input type="button" value=" 3 " class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value += '3'" />
+<input type="button" value=" + " class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value += '+'" />
 <br/>
       
-<input type="button" value=" 4 " class="button-no-animate-mode"onclick="calculator.answer.value += '4'" />
-<input type="button" value=" 5 " class="button-no-animate-mode" onclick="calculator.answer.value += '5'" />
-<input type="button" value=" 6 " class="button-no-animate-mode"onclick="calculator.answer.value += '6'" />
-<input type="button" value=" - " class="button-no-animate-mode"onclick="calculator.answer.value += '-'" />
+<input type="button" value=" 4 " class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value += '4'" />
+<input type="button" value=" 5 " class="FOIL-button-regular-no-shadow" onclick="calculator.answer.value += '5'" />
+<input type="button" value=" 6 " class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value += '6'" />
+<input type="button" value=" - " class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value += '-'" />
 </br>
     
-<input type="button" value=" 7 " class="button-no-animate-mode"onclick="calculator.answer.value += '7'" />
-<input type="button" value=" 8 "  class="button-no-animate-mode" onclick="calculator.answer.value += '8'" />
-<input type="button" value=" 9 "  class="button-no-animate-mode"onclick="calculator.answer.value += '9'" />
-<input type="button" value=" x "  class="button-no-animate-mode"onclick="calculator.answer.value += '*'" />
+<input type="button" value=" 7 " class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value += '7'" />
+<input type="button" value=" 8 "  class="FOIL-button-regular-no-shadow" onclick="calculator.answer.value += '8'" />
+<input type="button" value=" 9 "  class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value += '9'" />
+<input type="button" value=" x "  class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value += '*'" />
 </br>
   
-<input type="button" value=" c "  class="button-no-animate-mode"onclick="calculator.answer.value = ''" />
-<input type="button" value=" 0 "  class="button-no-animate-mode"onclick="calculator.answer.value += '0'" />
-<input type="button" value=" = "  class="button-no-animate-mode"onclick="calculator.answer.value = eval(calculator.answer.value)" />
-<input type="button" value=" / "  class="button-no-animate-mode"onclick="calculator.answer.value += '/'" />
+<input type="button" value=" c "  class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value = ''" />
+<input type="button" value=" 0 "  class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value += '0'" />
+<input type="button" value=" = "  class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value = eval(calculator.answer.value)" />
+<input type="button" value=" / "  class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value += '/'" />
 </br>
 
 
