1f7a94050f6c74d86eb96b9c1d8142e5d77c582d
senalbulumulle <senal.bulumulle@gmail.com>
2022-12-11 13:16:29 -0500

    Include [src/position-text.css] in calculator

--- APPS-Calculator/System_Files/system.global.app.include.ui.php
+++ APPS-Calculator/System_Files/system.global.app.include.ui.php
@@ -10,3 +10,4 @@
 <link rel="stylesheet" href="../src/calculator.css" type="text/css">
 <link rel="stylesheet" href="src/FOIL-UI.css" type="text/css">
 <link rel="stylesheet" href="src/config.css" type="text/css">
+<link rel="stylesheet" href="src/position-text.css" type="text/css">
