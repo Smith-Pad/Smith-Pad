8a6250b1d2a04f6847c7f14f2bf60b5029cf90b5
senalbulumulle <senal.bulumulle@gmail.com>
2022-12-11 13:20:15 -0500

    For the buttons, more spacing for more accessibility for touch

--- APPS-Calculator/System_Files/system.app.calculator.content.php
+++ APPS-Calculator/System_Files/system.app.calculator.content.php
@@ -7,28 +7,28 @@
 <div id='result'>
 <center>
 <form name="calculator">
-      
-<input type="text" name="answer" />
+<input type="text" name="answer" class="FOIL-button-regular-no-shadow" /> 
+
 <br>
-      
+<?php include 'src/avakasaya/space-5.php'; ?>          
 <input type="button" value=" 1 " class="FOIL-button-regular-no-shadow" onclick="calculator.answer.value += '1'" />
 <input type="button" value=" 2 " class="FOIL-button-regular-no-shadow" onclick="calculator.answer.value += '2'" />
 <input type="button" value=" 3 " class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value += '3'" />
 <input type="button" value=" + " class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value += '+'" />
 <br/>
-      
+<?php include 'src/avakasaya/space-7.php'; ?>
 <input type="button" value=" 4 " class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value += '4'" />
 <input type="button" value=" 5 " class="FOIL-button-regular-no-shadow" onclick="calculator.answer.value += '5'" />
 <input type="button" value=" 6 " class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value += '6'" />
 <input type="button" value=" - " class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value += '-'" />
 </br>
-    
+<?php include 'src/avakasaya/space-7.php'; ?>
 <input type="button" value=" 7 " class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value += '7'" />
 <input type="button" value=" 8 "  class="FOIL-button-regular-no-shadow" onclick="calculator.answer.value += '8'" />
 <input type="button" value=" 9 "  class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value += '9'" />
 <input type="button" value=" x "  class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value += '*'" />
 </br>
-  
+<?php include 'src/avakasaya/space-7.php'; ?>  
 <input type="button" value=" c "  class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value = ''" />
 <input type="button" value=" 0 "  class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value += '0'" />
 <input type="button" value=" = "  class="FOIL-button-regular-no-shadow"onclick="calculator.answer.value = eval(calculator.answer.value)" />
