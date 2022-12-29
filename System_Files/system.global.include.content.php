795a906a93de33bd4a44bc774a32f7082e45cfd9
senalbulumulle <senal.bulumulle@gmail.com>
2022-12-29 09:57:51 -0500

    Removed the subjects button for now...

--- System_Files/system.global.include.content.php
+++ System_Files/system.global.include.content.php
@@ -19,7 +19,6 @@ function content() {
         print('<script src="src/FOIL-time.js"></script>');
         print('<center><FOIL-font-size-40> Menu </FOIL-font-size-40></center>');
         print('<a href="#" class="FOIL-button-regular-no-shadow">Settings</a>');
-        print('<a href="Subjects/index.php" class="FOIL-button-regular-no-shadow">Subjects</a>');
         print('<a href="#" class="FOIL-button-regular-no-shadow">Apps</a>');
 }
 content();
